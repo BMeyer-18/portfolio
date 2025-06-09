19bda1e16cb6be27e404a6088808c97552e893fc
BMeyer-18 <bzmeyer18@gmail.com>
2025-06-09 00:32:53 -0400

    Create project-script.js

---
+++ script/project-script.js
@@ -0,0 +1,17 @@
+function newTitle(parent, name, img) {
+    parent.setAttribute("style", "display: flex; align-items: center; margin-top: 50px;");
+    parent.setAttribute("width", "100%");
+
+    const image = document.createElement("img");
+    image.setAttribute("src", img);
+    image.setAttribute("height", "70px");
+    parent.appendChild(image);
+
+    const title = document.createElement("h2");
+    title.innerHTML = name;
+    title.setAttribute("style", "margin-left: 40px; font-size: 50px;");
+    parent.appendChild(title);
+}
+
+const research = document.getElementById("research");
+newTitle(research, "Research", "./other/medal.png");
